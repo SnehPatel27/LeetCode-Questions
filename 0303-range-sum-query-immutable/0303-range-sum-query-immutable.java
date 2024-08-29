904bd018558289499257018bb96802bb51974eec
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-08-28 19:36:48 -0700

    Time: 8 ms (59.27%), Space: 49.3 MB (42.73%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -2,7 +2,7 @@ class NumArray {
     int num[];
     public NumArray(int[] nums) {
         for(int i=1; i<nums.length; i++){
-            nums[i] += nums[i-1];
+            nums[i] = nums[i] + nums[i-1];
         }
         this.num = nums;
     }
