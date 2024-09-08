24dffd709196ac0dfd0e370d524f079b41dd232a
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-09-08 13:07:01 -0700

    Time: 9 ms (26.95%), Space: 56.1 MB (93.06%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,30 +1,34 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), s, 0);
+
+        List<String> temp = new ArrayList<>();
+
+        checksubstrings(result, temp, 0, s);
+
         return result;
     }
-    
-    private void backtrack(List<List<String>> result, List<String> tempList, String s, int start) {
-        if (start >= s.length()) {
-            result.add(new ArrayList<>(tempList));
+
+    public void checksubstrings(List<List<String>> result, List<String> temp, int start, String s){
+        if(start >= s.length()){
+            result.add(new ArrayList<>(temp));
             return;
         }
-        
-        for (int end = start + 1; end <= s.length(); end++) {
-            String substring = s.substring(start, end);
-            if (isPalindrome(substring)) {
-                tempList.add(substring);
-                backtrack(result, tempList, s, end);
-                tempList.remove(tempList.size() - 1);
+
+        for(int end=start+1; end<=s.length(); end++){
+            if(isPalindrome(s.substring(start, end))){
+                temp.add(s.substring(start, end));
+                checksubstrings(result, temp, end, s);
+                temp.remove(temp.size() - 1);
             }
         }
     }
-    
-    private boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+
+    public Boolean isPalindrome(String s){
+        int left=0;
+        int right=s.length() - 1;
+        while(left < right){
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
             left++;
