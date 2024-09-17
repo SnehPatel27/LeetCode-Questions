2db67631a812df3de81f86189dd95fe2a1fce6a3
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-09-17 16:09:04 -0700

    Time: 165 ms (5.79%), Space: 48.2 MB (31.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement2(int[] nums) {
         List<Integer> result = new ArrayList<>();
         HashMap<Integer, Integer> count = new HashMap<>();
         for(int num: nums){
@@ -14,6 +14,60 @@ class Solution {
                 }
             }
         }
+
+        return result;
+    }
+    public List<Integer> majorityElement(int[] nums) {
+        
+        List<Integer> result = new ArrayList<>();
+
+        int count1=0;
+        int count2=0;
+        int candidate1 = 0;
+        int candidate2 = 0;
+
+        for(int num: nums){
+            if(num == candidate1){
+                count1++;
+            }else if(num == candidate2){
+                count2++;
+            }else if(count1 == 0){
+                candidate1 = num;
+                count1++;
+            }else if(count2 == 0){
+                candidate2 = num;
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        System.out.println(candidate1);
+        System.out.println(candidate2);
+
+        for(int num: nums){
+            if(num == candidate1){
+                count1++;
+            }
+            if(num == candidate2){
+                count2++;
+            }
+        }
+
+        if(count1 > nums.length/3){
+            if(count2 > nums.length/3 && candidate1 == candidate2){
+                result.add(candidate1);    
+                return result;
+            }
+            result.add(candidate1);
+        } 
+        if(count2 > nums.length/3){
+            result.add(candidate2);
+        }   
 
         return result;
     }
