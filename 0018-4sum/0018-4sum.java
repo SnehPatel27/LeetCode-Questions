2cba8d8b09fe0cff871c12a15fcf29dc1062bfae
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-09-24 23:07:12 -0700

    Time: 20 ms (35.02%), Space: 44.1 MB (44.68%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i < nums.length - 3; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for(int j = i + 1; j < nums.length - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                int leftpointer = j + 1;
+                int rightpointer = nums.length - 1;
+
+                while(leftpointer < rightpointer){
+                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[leftpointer] + (long)nums[rightpointer];
+                
+                    if(sum == (long)target){
+                        result.add(Arrays.asList(nums[i], nums[j], nums[leftpointer], nums[rightpointer]));
+
+                        while(leftpointer < rightpointer && nums[leftpointer] == nums[leftpointer + 1]){
+                        leftpointer++;
+                        }
+                        while(leftpointer < rightpointer && nums[rightpointer] == nums[rightpointer - 1]){
+                            rightpointer--;
+                        }
+                        leftpointer++;
+                        rightpointer--;
+                    }
+                    else if(sum > (long)target){
+                        rightpointer--;
+                    }else{
+                        leftpointer++;
+                    }
+                }    
+            }
+        }
+        return result;
+    }
+}
