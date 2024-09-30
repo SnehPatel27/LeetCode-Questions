413c5df4472756acb6b66b5fb4ffc0394e79f7de
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-09-29 22:16:46 -0700

    Time: 5 ms (87.54%), Space: 44.6 MB (46.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,17 +1,35 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int p=0;
-        int answer = 0;
-        int leftpointer = 0;
+        HashMap<Character, Integer> result = new HashMap<>();
+        int length = 0;
+        int maxlength = 0;
+        int startIndex = 0;
         for(int i=0;i<s.length();i++){
-            char currentChar = s.charAt(i);
-            if(map.containsKey(currentChar) && map.get(currentChar) >= leftpointer){
-                leftpointer = map.get(currentChar) + 1;
+            char c = s.charAt(i);
+            if(result.containsKey(c)){
+                startIndex = Math.max(result.get(c) + 1, startIndex);
             }
-            map.put(currentChar, i);
-            answer = Math.max(answer, i - leftpointer + 1);    
+            maxlength = Math.max(i - startIndex + 1, maxlength);
+            result.put(c, i);
         }
-        return Math.max(answer, p);
+        return Math.max(length, maxlength);
     }
 }
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         Map<Character, Integer> map = new HashMap<>();
+//         int p=0;
+//         int answer = 0;
+//         int leftpointer = 0;
+//         for(int i=0;i<s.length();i++){
+//             char currentChar = s.charAt(i);
+//             if(map.containsKey(currentChar) && map.get(currentChar) >= leftpointer){
+//                 leftpointer = map.get(currentChar) + 1;
+//             }
+//             map.put(currentChar, i);
+//             answer = Math.max(answer, i - leftpointer + 1);    
+//         }
+//         return Math.max(answer, p);
+//     }
+// }
