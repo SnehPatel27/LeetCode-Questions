74d9b711414edb3ff0be45369b62f6e9a376fd24
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-10-01 13:13:49 -0700

    Time: 1 ms (100%), Space: 44.2 MB (71.34%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,58 +10,29 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
         ListNode result = new ListNode(0);
         ListNode head = result;
 
         ListNode dummy = result;
-        int ans = 0;
         int carryover = 0;
-        while(l1!=null && l2!=null){
-            ans = l1.val + l2.val + carryover;
-            System.out.println(ans);
-            carryover = 0;
-            if(ans < 10){
-                dummy.next = new ListNode(ans);
-            }else{
-                carryover = 1;
-                dummy.next = new ListNode(ans%10);
-            }
+        while(l1!=null || l2!=null || carryover!=0){
+            
+            int value1 = l1!=null ? l1.val:0;
+            int value2 = l2!=null ? l2.val:0;
+
+            int ans = value1 + value2 + carryover;
+            carryover = ans/10;
+            int digit = ans % 10;
+
+            dummy.next = new ListNode(digit);
+
             dummy = dummy.next;
-            l1 = l1.next;
-            l2 = l2.next;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }   
 
-        while(l1!=null){
-            ans = l1.val + 0 + carryover;
-            System.out.println(ans);
-            carryover = 0;
-            if(ans < 10){
-                dummy.next = new ListNode(ans);
-            }else{
-                carryover = 1;
-                dummy.next = new ListNode(ans%10);
-            }
-            dummy = dummy.next;
-            l1 = l1.next;
-        }
-
-        while(l2!=null){
-            ans = 0 + l2.val + carryover;
-            System.out.println(ans);
-            carryover = 0;
-            if(ans < 10){
-                dummy.next = new ListNode(ans);
-            }else{
-                carryover = 1;
-                dummy.next = new ListNode(ans%10);
-            }
-            dummy = dummy.next;
-            l2 = l2.next;
-        }
-
-        if(carryover == 1){
-            dummy.next = new ListNode(1);
-        }
         return head.next;
     }
 }
