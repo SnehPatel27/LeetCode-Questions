cb76e5fa8df7e95279e5f08b2b3a8040243a0440
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-10-18 13:02:26 -0700

    Time: 5 ms (72.27%), Space: 57 MB (92.04%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,19 +1,35 @@
 class Solution {
     public int maxArea(int[] height) {
-        int leftp = 0;
-        int area = 0;
-        int rightp = height.length-1;
-        while(leftp < rightp){
-            int breadth = rightp - leftp;
-            int length = Math.min(height[leftp], height[rightp]);
-            area = Math.max(area, length * breadth);
+        // int leftp = 0;
+        // int area = 0;
+        // int rightp = height.length-1;
+        // while(leftp < rightp){
+        //     int breadth = rightp - leftp;
+        //     int length = Math.min(height[leftp], height[rightp]);
+        //     area = Math.max(area, length * breadth);
 
-            if(height[leftp] > height[rightp]){
-                rightp--;
+        //     if(height[leftp] > height[rightp]){
+        //         rightp--;
+        //     }else{
+        //         leftp++;
+        //     }
+        // }
+        // return area;
+
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+        int ans = Integer.MIN_VALUE;
+        while(left<right){
+            int distance = right - left;
+            ans = Math.max(ans, distance * (Math.min(height[left], height[right])));
+
+            if(height[left] <= height[right]){
+                left++;
             }else{
-                leftp++;
+                right--;
             }
         }
-        return area;
+        return ans;
     }
 }
