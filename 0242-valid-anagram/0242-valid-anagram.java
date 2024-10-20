f822b2052c13931eb79d8d64f3066a6089c41785
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-10-19 19:03:50 -0700

    Time: 9 ms (34.57%), Space: 43.3 MB (68.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,42 @@
 class Solution {
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length() != t.length()){
+    //         return false;
+    //     }
+
+    //     HashMap<Character, Integer> smap = new HashMap<>();
+    //     HashMap<Character, Integer> tmap = new HashMap<>();
+
+    //     for(int i = 0 ; i < s.length(); i++){
+    //         smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0) + 1);
+    //         tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0) + 1);
+    //     }
+    //     return smap.equals(tmap);
+    // }
+
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
+        s.toUpperCase();
+        t.toUpperCase();
 
-        HashMap<Character, Integer> smap = new HashMap<>();
-        HashMap<Character, Integer> tmap = new HashMap<>();
+        int count[] = new int[26];
 
-        for(int i = 0 ; i < s.length(); i++){
-            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0) + 1);
-            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0) + 1);
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
         }
-        return smap.equals(tmap);
+
+        for(int i = 0; i < t.length(); i++){
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int val: count){
+            if(val != 0 ){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
