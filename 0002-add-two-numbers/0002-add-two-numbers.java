0331237403ed9cc9621f6d0f1477b67467259c1a
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-11-08 21:10:13 -0800

    Time: 1 ms (100%), Space: 44.2 MB (70.57%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,28 +11,55 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
+        // ListNode result = new ListNode(0);
+        // ListNode head = result;
+
+        // ListNode dummy = result;
+        // int carryover = 0;
+        // while(l1!=null || l2!=null || carryover!=0){
+            
+        //     int value1 = l1!=null ? l1.val:0;
+        //     int value2 = l2!=null ? l2.val:0;
+
+        //     int ans = value1 + value2 + carryover;
+        //     carryover = ans/10;
+        //     int digit = ans % 10;
+
+        //     dummy.next = new ListNode(digit);
+
+        //     dummy = dummy.next;
+
+        //     if(l1 != null) l1 = l1.next;
+        //     if(l2 != null) l2 = l2.next;
+        // }   
+
+        // return head.next;
+
         ListNode result = new ListNode(0);
         ListNode head = result;
+        ListNode dummy = head;
 
-        ListNode dummy = result;
-        int carryover = 0;
-        while(l1!=null || l2!=null || carryover!=0){
-            
-            int value1 = l1!=null ? l1.val:0;
-            int value2 = l2!=null ? l2.val:0;
+        int carry = 0;
 
-            int ans = value1 + value2 + carryover;
-            carryover = ans/10;
-            int digit = ans % 10;
+        while(l1!=null || l2!=null || carry!=0){
 
-            dummy.next = new ListNode(digit);
+            int value1 =  (l1 != null) ? l1.val:0;
+            int value2 =  (l2 != null) ? l2.val:0;
 
+            int sum = value1 + value2 + carry;
+
+            int ans = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(ans);
+            dummy.next = newNode;
             dummy = dummy.next;
 
             if(l1 != null) l1 = l1.next;
             if(l2 != null) l2 = l2.next;
-        }   
+        }
 
         return head.next;
+
     }
 }
