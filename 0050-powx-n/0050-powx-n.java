2b184a9458f871a7f22f23cb99ee2bd250088003
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-11-08 22:15:47 -0800

    Time: 0 ms (100%), Space: 41.8 MB (80.96%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        
+        return calPower(x, (long) n);
+    }
+
+    public double calPower(double x, long n){
+        if(n == 0) return 1;
+
+        if(n < 0) return 1 / calPower(x, -n);
+
+        if(n % 2 == 1){
+            return x * calPower(x * x, (n - 1)/2);
+        }else{
+            return calPower(x * x, (n/2));
+        }
     }
 }
