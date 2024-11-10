0550683358a6ddb3230093338260f16f409ed8d6
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-11-09 23:11:29 -0800

    Time: 2 ms (72.07%), Space: 41.8 MB (95.99%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -0,0 +1,59 @@
+class Pair{
+    int row;
+    int col;
+    int time;
+    Pair(int row, int col, int time){
+        this.row = row;
+        this.col = col;
+        this.time = time;
+    }
+}
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] visited = new int[rows][cols];
+        int freshCount = 0;
+
+        Queue<Pair> que = new LinkedList<>();
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 2){
+                    visited[i][j] = 2;
+                    que.add(new Pair(i, j, 0));
+                }else{
+                    visited[i][j] = 0;
+                }
+
+                if(grid[i][j] == 1) freshCount++;
+            }
+        }
+
+        int[] r = new int[]{-1, 0, 1, 0};
+        int[] c = new int[]{0, 1, 0, -1};
+        int t = 0, count = 0;
+
+        while(!que.isEmpty()){
+            int currentRow = que.peek().row;  
+            int currentCol = que.peek().col;
+            t = que.peek().time;
+            que.poll();
+
+            for(int j = 0; j < 4; j++){
+                int neighborRow = currentRow + r[j];
+                int neighborCol = currentCol + c[j];
+
+                if(neighborRow >= 0 && neighborRow < rows && neighborCol >= 0 && neighborCol < cols && visited[neighborRow][neighborCol] == 0 && grid[neighborRow][neighborCol] == 1){
+                    visited[neighborRow][neighborCol] = 2;
+                    que.add(new Pair(neighborRow, neighborCol, t + 1));
+                    count++;
+                }
+            }
+        }
+
+        if(freshCount == count) return t;
+        else return -1;
+    }
+}
