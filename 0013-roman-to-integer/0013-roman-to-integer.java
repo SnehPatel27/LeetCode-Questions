745b7f24fcbe0ee5553da11becb616e3679ea123
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-11-10 22:25:31 -0800

    Time: 5 ms (63.03%), Space: 44.4 MB (91.86%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -14,7 +14,8 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             char currentChar = s.charAt(i);
             if(i+1 < s.length() && mapping.get(currentChar) < mapping.get(s.charAt(i + 1))){
-                num -= mapping.get(currentChar); 
+                num += mapping.get(s.charAt(i + 1)) - mapping.get(currentChar); 
+                i++;
             }else{
                 num += mapping.get(currentChar);
             }
