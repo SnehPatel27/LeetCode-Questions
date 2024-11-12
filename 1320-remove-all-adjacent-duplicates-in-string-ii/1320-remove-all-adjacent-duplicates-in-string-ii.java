7d8133ae7a32518bbcb90eb8ed85292fba828821
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-11-11 22:15:02 -0800

    Time: 38 ms (45.29%), Space: 45.3 MB (65.45%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,38 @@
+class Pair{
+    char character;
+    int value;
+    Pair(char character, int value){
+        this.character = character;
+        this.value = value;
+    }
+}
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(!st.isEmpty() && st.peek()[0] == c){
+
+                st.peek()[1] = st.peek()[1] + 1;    
+
+                if(st.peek()[1] == k){
+                    st.pop();
+                }
+
+            } else {
+                st.push(new int[]{c, 1});
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while(!st.isEmpty()){
+            result.append(String.valueOf((char)st.peek()[0]).repeat(st.peek()[1]));
+            st.pop();
+        }
+
+        return result.reverse().toString();
+    }
+}
