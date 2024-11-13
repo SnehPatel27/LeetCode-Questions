57ec7dee14636e2ef958febbf9a59dce41ce4086
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2024-11-12 23:07:47 -0800

    Time: 27 ms (84.37%), Space: 51.5 MB (47.84%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSumm(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length-1;i++){
@@ -26,6 +26,43 @@ class Solution {
                 }
             }
         }
+        return answer;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> answer = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 1; i++){
+
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            int leftp = i + 1;
+            int rightp = nums.length - 1;
+
+            while(leftp < rightp){
+                int total = nums[i] + nums[leftp] + nums[rightp];
+                
+                if(total < 0) leftp++;
+
+                else if(total > 0) rightp--;
+
+                else {
+                    answer.add(Arrays.asList(nums[i], nums[leftp], nums[rightp]));
+
+                    leftp++;
+
+                    while(leftp < rightp && nums[leftp] == nums[leftp - 1]){
+                        leftp++;
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 }
