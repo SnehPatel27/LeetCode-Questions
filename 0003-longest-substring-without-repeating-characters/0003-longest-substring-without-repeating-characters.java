c88f95d13b4a14ed4c251baa02371f8584b5eb38
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-05 00:37:07 +0530

    Time: 5 ms (87.1%), Space: 45.1 MB (21.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,18 +1,5 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // HashMap<Character, Integer> result = new HashMap<>();
-        // int length = 0;
-        // int maxlength = 0;
-        // int startIndex = 0;
-        // for(int i=0;i<s.length();i++){
-        //     char c = s.charAt(i);
-        //     if(result.containsKey(c)){
-        //         startIndex = Math.max(result.get(c) + 1, startIndex);
-        //     }
-        //     maxlength = Math.max(i - startIndex + 1, maxlength);
-        //     result.put(c, i);
-        // }
-        // return Math.max(length, maxlength);
 
         HashMap<Character, Integer> result = new HashMap<>();
         int left = 0;
@@ -22,9 +9,11 @@ class Solution {
         int n = s.length();
         while(right < n){
             if(result.containsKey(s.charAt(right))){
-                if(result.get(s.charAt(right)) >= left){
-                    left = result.get(s.charAt(right)) + 1;
-                }
+                // if(result.get(s.charAt(right)) >= left){
+                //     left = result.get(s.charAt(right)) + 1;
+                // }
+
+                left = Math.max(result.get(s.charAt(right)) + 1, left);
             }
             length = right - left + 1;
             maxlen = Math.max(length, maxlen);
@@ -35,21 +24,3 @@ class Solution {
         return maxlen;
     }
 }
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         Map<Character, Integer> map = new HashMap<>();
-//         int p=0;
-//         int answer = 0;
-//         int leftpointer = 0;
-//         for(int i=0;i<s.length();i++){
-//             char currentChar = s.charAt(i);
-//             if(map.containsKey(currentChar) && map.get(currentChar) >= leftpointer){
-//                 leftpointer = map.get(currentChar) + 1;
-//             }
-//             map.put(currentChar, i);
-//             answer = Math.max(answer, i - leftpointer + 1);    
-//         }
-//         return Math.max(answer, p);
-//     }
-// }
