8f27bd508e6ae1719d5fb1ae2c9f1c2e0d2769c2
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-05 18:44:06 +0530

    Time: 51 ms (48.26%), Space: 55.2 MB (45.97%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
+    //In case of Inteview please show this 
+    public int totalFruitt(int[] fruits) {
         HashMap<Integer, Integer> result = new HashMap<>();
         int n = fruits.length;
 
@@ -24,6 +25,40 @@ class Solution {
             }
 
             maxLen = Math.max(maxLen, rightPointer - leftPointer + 1);
+            
+            rightPointer++;
+        }
+
+        return maxLen;
+    }
+
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        int n = fruits.length;
+
+        int leftPointer = 0;
+        int rightPointer = 0;
+
+        int maxLen = 0;
+
+        while(rightPointer < n){
+            
+            result.put(fruits[rightPointer], result.getOrDefault(fruits[rightPointer], 0) + 1);
+
+            if(result.size() > 2){
+                int leftFruit = fruits[leftPointer];
+                result.put(leftFruit, result.get(leftFruit) - 1);
+
+                if(result.get(leftFruit) == 0){
+                    result.remove(leftFruit);
+                }
+
+                leftPointer++;
+            }
+
+            if(result.size() <= 2){
+                maxLen = Math.max(maxLen, rightPointer - leftPointer + 1);
+            }
             
             rightPointer++;
         }
