bfa4d15994b7bf690997b10d7cc5a0999c1f8ce6
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-05 18:41:24 +0530

    Time: 52 ms (38.22%), Space: 55.7 MB (17.8%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        int n = fruits.length;
+
+        int leftPointer = 0;
+        int rightPointer = 0;
+
+        int maxLen = 0;
+
+        while(rightPointer < n){
+            
+            result.put(fruits[rightPointer], result.getOrDefault(fruits[rightPointer], 0) + 1);
+
+            while(result.size() > 2){
+                int leftFruit = fruits[leftPointer];
+                result.put(leftFruit, result.get(leftFruit) - 1);
+
+                if(result.get(leftFruit) == 0){
+                    result.remove(leftFruit);
+                }
+
+                leftPointer++;
+            }
+
+            maxLen = Math.max(maxLen, rightPointer - leftPointer + 1);
+            
+            rightPointer++;
+        }
+
+        return maxLen;
+    }
+}
