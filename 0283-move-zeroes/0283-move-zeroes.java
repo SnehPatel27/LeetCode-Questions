9fab358d316c7866a940fc949c6eb9fa4d49b5ad
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-10 14:26:38 +0530

    Time: 2 ms (85.26%), Space: 46 MB (40.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     
     //Bute Force Solution
-    public void moveZeroes(int[] nums) {
+    public void moveZeroess(int[] nums) {
         List<Integer> temp = new ArrayList<>();
         int n = nums.length;
         
@@ -17,6 +17,31 @@ class Solution {
 
         for(int i = temp.size(); i < n; i++){
             nums[i] = 0;
+        }
+    }
+
+    //optimal Approach
+    public void moveZeroes(int[] nums) {
+        
+        int j = -1;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1) return;
+
+        for(int i = j + 1; i < n; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                j++;
+            }
         }
     }
 }
