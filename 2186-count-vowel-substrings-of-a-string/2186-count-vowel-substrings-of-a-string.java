35366b7b363abb6df414c2200e1fe6dd59a1d014
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-10 13:52:33 +0530

    Time: 170 ms (5.08%), Space: 45.1 MB (10.17%) - LeetHub

--- 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
@@ -1,53 +1,29 @@
 class Solution {
 
-    // public Boolean isVowel(Character c){
-    //     return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    // }
+    public Boolean isVowel(Character c){
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
 
-    // public int countVowelSubstrings(String word) {
+    public int countVowelSubstrings(String word) {
         
-    //     int n = word.length();
-    //     int ans = 0;
-        
-    //     for(int i = 0; i < n; i++){
-    //         Map<Character, Integer> freq = new HashMap<>();
-    //         for(int j = i; j < n; j++){
-    //             if(isVowel(word.charAt(j))){
-    //                 freq.put(word.charAt(j), freq.getOrDefault(word.charAt(j), 0) + 1);
-    //                 if(freq.size() == 5){
-    //                     System.out.println(word.substring(i, j + 1));
-    //                     ans++;
-    //                 }
-    //             } else {
-    //                 break;
-    //             }
-    //         }
-    //     }
-
-    //     return ans;
-    // }
-
-    public int countVowelSubstrings(String word)
-    {
-        int vow = 0;
         int n = word.length();
-        Set<Character> set = new HashSet<>();
-        for(int i = 0; i < n-4; i++)
-        {
-            set.clear();
-            for(int j = i; j < n; j++)
-            {
-                char ch = word.charAt(j);
-                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                {
-                    set.add(ch);
-                    if(set.size() == 5)
-                        vow++;
-                }
-                else
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++){
+            Map<Character, Integer> freq = new HashMap<>();
+            for(int j = i; j < n; j++){
+                if(isVowel(word.charAt(j))){
+                    freq.put(word.charAt(j), freq.getOrDefault(word.charAt(j), 0) + 1);
+                    if(freq.size() == 5){
+                        System.out.println(word.substring(i, j + 1));
+                        ans++;
+                    }
+                } else {
                     break;
+                }
             }
         }
-        return vow;
+
+        return ans;
     }
 }
