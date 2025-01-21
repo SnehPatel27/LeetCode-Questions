60531fbb858710641416fb507272f73f2a1531b6
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-21 15:07:52 +0530

    Time: 1542 ms (5%), Space: 47.1 MB (10.88%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,4 +1,16 @@
 class Solution {
+
+    //NeetCode
+
+    /* Iterate over the entire array and for each element conpute the strings that can be formed with that character as the 
+    middle element. There would be a hashset that would store the strings that can be formed. The output would be the 
+    size of the set. */
+
+    /* Initially there would be the hashmap that would store all the frequencies of each character. */
+
+    /* For the first encountered character check if there is any element present in the left hash set and the hashmap both.
+    If yes then that 1 string can be formed. but before that remember to decrease the frequency of that element from the right hashmap.  */
+
     public int countPalindromicSubsequence(String s) {
         
         Map<Character, Integer> res = new HashMap<>();
