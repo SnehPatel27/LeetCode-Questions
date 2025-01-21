1e36ccfc3704904bd610be9ccb5eafe282e3a1af
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-21 13:27:58 +0530

    Time: 4 ms (65.5%), Space: 53.4 MB (55.32%) - LeetHub

---
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        
+        int currentMax = values[0] - 1;
+        int max = Integer.MIN_VALUE;
+
+        //Keep the track of the maximum element on the left and keep on moving forward.
+        //At every iteration the max is decremented by 1 because j - i increases by 1.
+
+        for(int i = 1; i < values.length; i++){
+            max = Math.max(max, values[i] + currentMax);
+            currentMax = Math.max(currentMax - 1, values[i] - 1);
+        }
+
+        return max;
+    }
+}
