6b99ff3f3b110794ef9672baa273ed8068446fb7
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-26 15:41:52 +0530

    Time: 0 ms (100%), Space: 41.3 MB (35.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,17 +4,16 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             dp[i] = -1;
         }
-        return checkAdjacentElements(nums.length - 1, nums, dp);
+        return checkAdjacentElements(0 , nums, dp);
     }
 
     public int checkAdjacentElements(int index, int[] nums, int[] dp){
-        if(index == 0) return nums[0];
-        else if(index < 0) return 0;
+        if(index >= nums.length) return 0;
 
         if(dp[index] != -1) return dp[index];
 
-        int pick = nums[index] + checkAdjacentElements(index - 2, nums, dp);
-        int notPick = checkAdjacentElements(index - 1, nums, dp);
+        int pick = nums[index] + checkAdjacentElements(index + 2, nums, dp);
+        int notPick = checkAdjacentElements(index + 1, nums, dp);
 
         return dp[index] = Math.max(pick, notPick);
     }
