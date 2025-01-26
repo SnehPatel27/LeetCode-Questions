4248f742e0ed1ed0f0251f427a2b603951589590
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-27 00:11:03 +0530

    Time: 2 ms (82.64%), Space: 43.8 MB (99.36%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,6 +1,11 @@
 class Solution {
     
     //This solution is only for the case when nums.length <= 100 and nums[i] <= 100
+    //Since there are only 100 nos and all the nos are said to be less than 100
+    //We iterate from the back and continue moving backward till we dont get the first duplicate from the back. 
+    //First duplicate indicates that we would have to delete till that number
+    //So then the remaining elements would be i + 1.
+    //So to remove those i + 1 elements the number of operation that would be required is i + 1 / 3.
     public int minimumOperations(int[] nums) {
         int[] freq = new int[101];
         Arrays.fill(freq, 0);
