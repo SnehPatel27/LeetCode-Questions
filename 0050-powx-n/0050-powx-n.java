08313b9651ad150029048c6fff2e16b025a23160
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-27 17:33:35 +0530

    Time: 0 ms (100%), Space: 42.8 MB (24.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,4 +1,25 @@
 class Solution {
+
+    //This is the brute force solution 
+    public double BruteForce(double x, int n) {
+
+        if(n == 0) return 1;
+
+        boolean negative = (n < 0)? true: false;
+        double num = x;
+        n = Math.abs(n);
+        while(n > 1){
+            x = x * num;
+            n--;
+        }
+
+        if(negative)
+            return 1/x;
+        else 
+            return x;
+    }   
+
+    //This is the optimal approach
     public double myPow(double x, int n) {
         
         return calPower(x, (long) n);
