c3a3e94c81908cbea07beb189f66f5adcb50a03e
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-27 11:44:51 +0530

    Time: 1 ms (100%), Space: 43.2 MB (46.94%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.java
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.java
@@ -2,11 +2,23 @@ class Solution {
 
     //This particular function fails for the condition when the * is at the star of the string p. 
     //For eg when s = "jjv" p = "*j"
+    //Because here even if the * is at the first index you tend to start the suffix search after p.length() spaces. 
+    //instead you should do it after index spaces.
+    public boolean hasMatchh(String s, String p) {
+        int index = p.indexOf('*');
+
+        int firstPosition = s.indexOf(p.substring(0, index));
+        int secondPosition = s.indexOf(p.substring(index + 1), firstPosition + p.length());
+
+        return (firstPosition != -1 && secondPosition != -1);
+    }
+
+    //In case of interview provide me this code
     public boolean hasMatch(String s, String p) {
         int index = p.indexOf('*');
 
         int firstPosition = s.indexOf(p.substring(0, index));
-        int secondPosition = s.indexOf(p.substring(index + 1), firstPosition + p.substring(0, index).length());
+        int secondPosition = s.indexOf(p.substring(index + 1), firstPosition + index);
 
         return (firstPosition != -1 && secondPosition != -1);
     }
