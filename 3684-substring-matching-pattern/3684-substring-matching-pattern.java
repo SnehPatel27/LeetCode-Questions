c81338b8f5382d8635a13ce3d88f5b5b13db6ea5
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-27 11:42:50 +0530

    Time: 1 ms (100%), Space: 42.9 MB (84.59%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.java
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.java
@@ -1,9 +1,12 @@
 class Solution {
+
+    //This particular function fails for the condition when the * is at the star of the string p. 
+    //For eg when s = "jjv" p = "*j"
     public boolean hasMatch(String s, String p) {
         int index = p.indexOf('*');
 
         int firstPosition = s.indexOf(p.substring(0, index));
-        int secondPosition = s.indexOf(p.substring(index + 1), firstPosition + index);
+        int secondPosition = s.indexOf(p.substring(index + 1), firstPosition + p.substring(0, index).length());
 
         return (firstPosition != -1 && secondPosition != -1);
     }
