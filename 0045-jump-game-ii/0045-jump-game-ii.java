cf70edf0222305378c1f32782c02e9e7432978c0
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-28 12:20:02 +0530

    Time: 1 ms (99.42%), Space: 44.9 MB (92.82%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,53 @@
 class Solution {
+    //This is the optimal approach. 
     public int jump(int[] nums) {
         
-        int near = 0, far = 0, jumps = 0;
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+        //For each standing index I would compute the farthest that I can go and the starting point would be the left and 
+        //the farthest would be the right. 
+        //For instance [2,3,1,1,4] standing at 2. Farthest I can go is right = 1 and the least i can go is left = 3 with jumps = 1.
+        //Then I would have a loop from left to right and calculate again the farthest I can go and the least I can go. 
+        //Least would always be right + 1. so you can direct make the left pointer as right + 1. 
+        //And the right pointer as farthest that you compute.
+        //You would do this till the right pointer reaches or exceed n - 1;
 
-        while(far < nums.length - 1){
+        while(right < nums.length - 1){
             int farthest = 0;
-            for(int i = near; i <= far; i++){
+
+            for(int i = left; i <= right; i++){
                 farthest = Math.max(farthest, i + nums[i]);
             }
 
-            near = far + 1;
-            far = farthest;
+            left = right + 1;
+            right = farthest;
             jumps++;
         }
+
         return jumps;
+
+    }
+
+    //Below two functions are the recursive solution where i find all the optimal paths and then figure out the min.
+    //In the worst case (e.g., when nums[i] = n for all i), the solution explores O(n^n) paths.
+    //Recursive Depth of o(n).
+    //You can optmize this with memoization to make the soln O(nsquare).
+    //Create a dp Array of n * n;
+    public int jumpRecursive(int[] nums) {
+        
+        return jumpRecursiveHelper(nums, 0, 0);
+    }
+
+    public int jumpRecursiveHelper(int[] nums, int index, int jumps){
+
+        if(index >= nums.length - 1) return jumps;
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i <= nums[index]; i++){
+            min = Math.min(min, jumpRecursiveHelper(nums, index + i, jumps + 1));
+        }
+
+        return min;
     }
 }
