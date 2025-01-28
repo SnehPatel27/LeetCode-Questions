4b39150930c507cac2679e9c32045a403f7e8676
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-28 16:04:57 +0530

    Time: 14 ms (36.03%), Space: 45.3 MB (64.94%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.java
@@ -1,4 +1,11 @@
 class Solution {
+    //We want two substrings - 1 containing all 0s and other containing all 1s.
+    //first start with a single right window which should be all 1s
+    //Then count the number of 0s.
+    //Then iterate over the entire string one by one and start placing one char from the right window to the left.
+    //if the char is 0. res = res - 1;
+    //if char is 1 res = res + 1;
+    //calculate the min at each iteration
     public int minFlipsMonoIncr(String s) {
         
         int res = 0;
