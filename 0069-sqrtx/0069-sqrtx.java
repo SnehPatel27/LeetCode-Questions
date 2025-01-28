6a2d7b2c6afe89e6a287babb29b65c181c9063c6
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-28 20:11:38 +0530

    Time: 56 ms (5.04%), Space: 40.9 MB (68.52%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,11 +1,13 @@
 class Solution {
     
     //This is the brute force solution 
+    //You could have written i * i <= x but for large value of i it will cause an overflow. 
+    //Hence write x/i.
     public int mySqrt(int x) {
         if(x == 1) return 1;
         int ans = 0;
         for(int i = 1; i < x; i++){
-            if(i  <= x/i)
+            if(i <= x/i)
                 ans = i;
             else
                 break;
