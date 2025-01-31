8ca29c536a4d25f34bc7381e1d0fb35d05f055bc
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-01-31 16:10:48 +0530

    Time: 2 ms (38.11%), Space: 44.8 MB (5.01%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    //This is the recursive solution with TLE
+    public int mincostTickets(int[] days, int[] costs) {
+        Integer[][] dp = new Integer[days.length][396];
+        return findMinCost(0, 0, days, costs, dp);   
+    }
+
+    public int findMinCost(int index,int daysCovered, int[] days, int[] costs, Integer[][] dp){
+
+        int oneDayPassPrice = 0, sevenDayPassPrice = 0, thirtyDayPassPrice = 0;
+
+        if(index >= days.length) return 0;
+
+        if(dp[index][daysCovered] != null) return dp[index][daysCovered];
+
+        if(days[index] <= daysCovered){
+            return dp[index][daysCovered] = findMinCost(index + 1, daysCovered, days, costs, dp);
+        }
+
+        
+        oneDayPassPrice = costs[0] + findMinCost(index + 1, days[index], days, costs, dp);
+        sevenDayPassPrice = costs[1] + findMinCost(index + 1, days[index] + 6, days, costs, dp);
+        thirtyDayPassPrice = costs[2] + findMinCost(index + 1, days[index] + 29, days, costs, dp);
+        
+
+        return dp[index][daysCovered] = Math.min(oneDayPassPrice, Math.min(sevenDayPassPrice, thirtyDayPassPrice));
+    }
+}
