9f13b2bbe94391e3b554f4201141ff0a5ab3cca6
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-09 01:48:56 +0530

    Time: 2 ms (98.06%), Space: 42 MB (41.68%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,5 +1,7 @@
 class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
+
+    //This is using a boolean array
+    public boolean uniqueOccurrences1(int[] arr) {
         HashMap<Integer, Integer> freq = new HashMap<>();
 
         for(int i = 0; i < arr.length; i++){
@@ -17,5 +19,18 @@ class Solution {
         }
 
         return true;
+    }
+
+    //This is using a hashSet
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++){
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
+        }
+
+        HashSet<Integer> res = new HashSet<>(freq.values());
+
+        return (res.size() == freq.size());
     }
 }
