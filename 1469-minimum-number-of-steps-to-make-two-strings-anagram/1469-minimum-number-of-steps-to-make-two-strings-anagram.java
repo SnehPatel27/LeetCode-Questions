e20fc064d952ebab78a8e3d6db3eae68683ced3a
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-10 02:13:10 +0530

    Time: 9 ms (49.43%), Space: 45.5 MB (35.57%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -3,6 +3,9 @@ class Solution {
         int[] freq = new int[26];
         int ans = 0;
 
+        //Count all the characters in t that are not present in s.
+        //It means we are getting those characters that we need to remove.
+        //number of characters to remove would be equal to number of chars to add
         for(int i = 0; i < s.length(); i++){
             freq[t.charAt(i) - 'a']++;
             freq[s.charAt(i) - 'a']--;
