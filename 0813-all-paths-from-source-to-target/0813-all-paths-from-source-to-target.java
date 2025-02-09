033d495e0a9314ea4d160249412bb6d0cc2f9ebc
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-10 01:42:51 +0530

    Time: 1 ms (100%), Space: 46.5 MB (37.42%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public void findAllPaths(List<List<Integer>> paths, List<Integer> temp, int source, int target, int[][] graph){
+
+        if(source == target){
+            paths.add(new ArrayList<>(temp));
+            return; 
+        }
+        
+        
+        for(int neighbor: graph[source]){
+            temp.add(neighbor);
+            findAllPaths(paths, temp, neighbor, target, graph);
+            temp.remove(temp.size() - 1);
+        }
+        
+    }
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer> temp = new ArrayList<>();
+        List<List<Integer>> paths = new ArrayList<>();
+
+        int n = graph.length;
+
+        temp.add(0);
+
+        findAllPaths(paths, temp, 0, n - 1, graph);
+
+        return paths;
+    }
+}
