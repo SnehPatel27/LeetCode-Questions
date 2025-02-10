850b3738119aaeb3b1555bd72d066f8b4ec00aa8
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-11 01:17:25 +0530

    Time: 48 ms (29.63%), Space: 44.3 MB (66.63%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
@@ -4,8 +4,7 @@ class Solution {
         int count = 0;
         List<int[]> list = new ArrayList<>();
         for(int i = lo; i <= hi; i++){
-            int[] ans = new int[1];
-            list.add(new int[]{i, findPowerValue(i, ans)});
+            list.add(new int[]{i, findPowerValue(i)});
         }
 
         list.sort((a,b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
@@ -13,18 +12,14 @@ class Solution {
         return list.get(k - 1)[0];
     }
 
-    public int findPowerValue(int x, int[] ans){
+    public int findPowerValue(int x){
         if(x == 1)
-            return ans[0];
+            return 0;
 
         if(x % 2 == 0){
-            ans[0] = ans[0] + 1;
-            findPowerValue(x/2, ans);
+            return 1 + findPowerValue(x/2);
         } else {
-            ans[0] = ans[0] + 1;
-            findPowerValue(3 * x + 1, ans);
+            return 1 + findPowerValue(3 * x + 1);
         }
-
-        return ans[0];
     }
 }
