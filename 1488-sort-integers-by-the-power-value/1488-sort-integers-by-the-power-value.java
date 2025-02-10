a6023b18642c61c8681f51bfeda4baa3239f2c9e
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-11 01:20:27 +0530

    Time: 39 ms (60.5%), Space: 44.4 MB (58.87%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
@@ -11,15 +11,27 @@ class Solution {
 
         return list.get(k - 1)[0];
     }
-
-    public int findPowerValue(int x){
+    //This solution uses recursion
+    public int findPowerValue1(int x){
         if(x == 1)
             return 0;
 
         if(x % 2 == 0){
-            return 1 + findPowerValue(x/2);
+            return 1 + findPowerValue1(x/2);
         } else {
-            return 1 + findPowerValue(3 * x + 1);
+            return 1 + findPowerValue1(3 * x + 1);
         }
+    }
+
+    //This solution uses a normal iteration
+    public int findPowerValue(int x){
+        int steps = 0;
+
+        while(x != 1){
+            x = (x % 2 == 0) ? x / 2 : 3 * x + 1;
+            steps++;
+        }
+
+        return steps;
     }
 }
