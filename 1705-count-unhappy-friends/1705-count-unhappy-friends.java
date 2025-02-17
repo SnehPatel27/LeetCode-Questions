6df8539c9f8c0519119fb97341664b822f4c5002
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-16 18:23:33 -0800

    Time: 2 ms (96.64%), Space: 64.1 MB (63.03%) - LeetHub

--- 1705-count-unhappy-friends/1705-count-unhappy-friends.java
+++ 1705-count-unhappy-friends/1705-count-unhappy-friends.java
@@ -3,6 +3,9 @@ class Solution {
         
         int[][] rank = new int[n][n];
 
+
+        //Create a rank matrix which shows the preference of each with others.
+        //Lower rank indicates more preference
         for(int i = 0; i < preferences.length; i++){
             for(int j = 0; j < preferences[i].length; j++){
                 rank[i][preferences[i][j]] = j; 
@@ -11,11 +14,18 @@ class Solution {
 
         int[] pairedWith = new int[n];
 
+        //Create a paired array see who is paired with whom
         for(int[] pair: pairs){
             pairedWith[pair[0]] = pair[1];
             pairedWith[pair[1]] = pair[0];
         }
 
+        //If X is paired with Y
+        //Check all the friends (U) who are prefered more than Y. 
+        //U is paired with V.
+        //Check if preference of U for X is more than that for V.
+        //rank[u][x] < rank[u][v];
+        //If this is the case it means that X should be paired with U rather than Y.
         int unhappyCount = 0;
         for(int x = 0; x < n; x++){
             int y = pairedWith[x];
