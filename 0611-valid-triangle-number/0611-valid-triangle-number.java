12bc95b182e970a5872fc44f770f4b1a615407dd
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-20 19:07:56 -0800

    Time: 22 ms (99.74%), Space: 43.4 MB (63.38%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,3 +1,13 @@
+//2 pointer approach 
+//Sort the array
+//lets say the sorted array is 2,2,3,4
+//start k from end which we would consider as third side. 
+//take i = 0 and j as k - 1
+//Computer j - i if nums[j] + nums[i] > k
+//eg nums[i] = 2(first 2) and nums[j] = 3 and nums[k] = 4
+//2 + 3 > 4 so all pairs like another 2,3,4 would also be valid so did j - i;
+//decrease value of k and then check again till k >= 2.
+
 class Solution {
     public int triangleNumber(int[] nums) {
 
