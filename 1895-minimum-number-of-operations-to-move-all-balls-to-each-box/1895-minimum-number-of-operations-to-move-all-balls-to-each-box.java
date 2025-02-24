455c525486e2508e13465723b827e880114b556b
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-24 01:22:58 -0800

    Time: 3 ms (92.95%), Space: 44.7 MB (80.27%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] minOperations(String boxes) {
+    public int[] minOperations_Brute(String boxes) {
         int totalBalls = 0;
         for(int i = 0; i < boxes.length(); i++){
             if(boxes.charAt(i) == '1'){
@@ -25,5 +25,39 @@ class Solution {
         }
 
         return answer;
+    }
+
+    public int[] minOperations(String boxes){
+
+        int n = boxes.length();
+        int ballsToLeft = 0;
+
+        int[] prefixSumLeft = new int[n];
+        prefixSumLeft[0] = 0;
+
+        for(int i = 0; i < n; i++){
+            if(i > 0)
+                prefixSumLeft[i] = ballsToLeft + prefixSumLeft[i - 1];
+            if(boxes.charAt(i) == '1'){
+                ballsToLeft += 1;
+            }
+        }
+
+        int[] prefixSumRight = new int[n];
+        prefixSumRight[n - 1] = 0;
+        int ballsToRight = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(i < n - 1)
+                prefixSumRight[i] += prefixSumRight[i + 1] + ballsToRight;
+            if(boxes.charAt(i) == '1'){
+                ballsToRight += 1;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            prefixSumLeft[i] = prefixSumLeft[i] + prefixSumRight[i];
+        }
+
+        return prefixSumLeft;
     }
 }
