4d3b8144eed5be45c172d33718aa62ad410a5d74
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-25 23:46:10 -0800

    Time: 6 ms (39.48%), Space: 45 MB (96.38%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,29 +1,20 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
         int count = 0;
+        int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 1){
-                    count += getPerimeter(i, j, grid);
+                    for(int[] direction: directions){
+                        int newRow = i + direction[0];
+                        int newCol = j + direction[1];
+
+                        if(newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length || grid[newRow][newCol] == 0)
+                            count++;
+                    }
                 }
             }
         }
         return count;
-    }
-
-    public int getPerimeter(int row, int col, int[][] grid){
-        int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        int count = 0;
-        for(int[] direction: directions){
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-
-            if(newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length)
-                continue;
-            
-            if(grid[newRow][newCol] == 1) count++;
-        }
-
-        return 4 - count;
     }
 }
