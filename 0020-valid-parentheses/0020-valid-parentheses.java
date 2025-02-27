f7b9c53ba4a9c5c2db656f68994a3bad88841242
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-02-27 12:51:01 -0800

    Time: 4 ms (13.88%), Space: 41.6 MB (91.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,27 @@
 class Solution {
+
     public boolean isValid(String s) {
+        HashMap<Character, Character> brackets = new HashMap<>(){{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[');
+        }};
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(brackets.containsValue(c)){
+                st.push(c);
+            } else if(!st.isEmpty() && st.peek() == brackets.get(c)){
+                st.pop();
+            } else return false;
+        }
+
+        return st.isEmpty();
+
+    }
+
+    public boolean isValidP(String s) {
         Stack<Character> st = new Stack<>();
 
         for(int i = 0; i < s.length(); i++){
