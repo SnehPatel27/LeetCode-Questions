925a240fbb450d460746aa13ed62651c099fab94
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-02 14:54:04 -0800

    Time: 1 ms (98.94%), Space: 45 MB (41.17%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+        List<int[]> mergedInterval = new ArrayList<>();
+        int i = 0;
+        
+        //Initially try adding all the intervals that are smaller than the newInterval.
+        //Also check the condition that i is less the total length of interval
+        while(i < n && intervals[i][1] < newInterval[0]){
+            mergedInterval.add(intervals[i]);
+            i++;
+        }
+
+        //Iterate now till there is merge in the intervals.
+        while(i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        mergedInterval.add(newInterval);
+
+        while(i < n){
+            mergedInterval.add(intervals[i]);
+            i++;
+        }
+
+        return mergedInterval.toArray(new int[mergedInterval.size()][]);
+    }
+}
