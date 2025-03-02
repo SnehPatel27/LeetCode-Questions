57bb6a5f9e74dc9c95162a8390f677a75434e702
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-02 15:07:55 -0800

    Time: 0 ms (100%), Space: 41.8 MB (64.9%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int lengthOfLastWord(String s) {
+    public int lengthOfLastWord1(String s) {
         int i = s.length() - 1;
         int count = 0;
         while(i >= 0){
@@ -14,5 +14,22 @@ class Solution {
         }
 
         return count;
+    }
+
+    //Editorial Solution
+    public int lengthOfLastWord(String s) {
+        int p = s.length(), length = 0;
+        while (p > 0) {
+            p--;
+            // we're in the middle of the last word
+            if (s.charAt(p) != ' ') {
+                length++;
+            }
+            // here is the end of last word
+            else if (length > 0) {
+                return length;
+            }
+        }
+        return length;
     }
 }
