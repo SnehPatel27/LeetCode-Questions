f64155fbb12dc493c8cbce5b7a1d89e357bb598e
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-04 16:28:14 -0800

    Time: 4 ms (71.1%), Space: 44.4 MB (77.95%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -19,8 +19,6 @@ class Solution {
                 public int compare(ListNode o1, ListNode o2){
                     if(o1.val > o2.val){
                         return 1;
-                    } else if(o1.val == o2.val){
-                        return 0;
                     } else return -1;
                 }
             }
