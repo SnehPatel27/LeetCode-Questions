c5e7ab0ae4e31cde5350d2a8e150aed122cf7f59
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-04 16:28:52 -0800

    Time: 4 ms (71.1%), Space: 43.9 MB (99.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -13,12 +13,15 @@ class Solution {
         ListNode mergedList = new ListNode(0);
         ListNode curr = mergedList;
 
+        // In this even if i do not mention about o1.val == o2.val return 0. It would still work
         PriorityQueue<ListNode> pq = new PriorityQueue<>(
             new Comparator<ListNode>(){
                 @Override
                 public int compare(ListNode o1, ListNode o2){
                     if(o1.val > o2.val){
                         return 1;
+                    } else if(o1.val == o2.val){
+                        return 0;
                     } else return -1;
                 }
             }
