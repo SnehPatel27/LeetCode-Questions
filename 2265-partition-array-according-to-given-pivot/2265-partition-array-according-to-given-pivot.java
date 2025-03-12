b9bbbe69a1051dffd1726f06326b1578a84ea030
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-12 15:12:38 -0700

    Time: 5 ms (76.01%), Space: 68.6 MB (9.13%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
+    //This approach involves using two seperate lists for greater and smaller elements.
+    //It then creates a resultant array from that. 
+    public int[] pivotArray1(int[] nums, int pivot) {
         int n = nums.length;
 
         List<Integer> smallerNumbers = new ArrayList<>();
@@ -29,6 +31,38 @@ class Solution {
 
         for(int i = 0; i < biggerNumbers.size(); i++){
             result[counter++] = biggerNumbers.get(i);
+        }
+
+        return result;
+    }
+
+    //The next approach is to use two pointer. 
+    //One pointer starts from front that tracks the smaller elements and increments after each correct placing. 
+    //One pointer starts from back that tracks the larger elements and decrements after each correct placing. 
+    //after setting all the smaller and the larger elements in the correct position.
+    //All the elements betweent the two pointer should be set to the pivot. 
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        int[] result = new int[n];
+
+        for(int i = 0, j = n - 1; i < n && j >= 0; i++, j--){
+            if(nums[i] < pivot){
+                result[left] = nums[i];
+                left++;
+            }
+
+            if(nums[j] > pivot){
+                result[right] = nums[j];
+                right--;
+            }
+        }
+
+        while(left <= right){
+            result[left] = pivot;
+            left++;
         }
 
         return result;
