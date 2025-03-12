f76774dab33cb68095f8a445cff72075177d7796
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-11 20:11:53 -0700

    Time: 9 ms (94.59%), Space: 46 MB (29.67%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,5 +1,6 @@
 class Solution {
-    public String removeKdigits(String num, int k) {
+    //This is my solution
+    public String removeKdigits1(String num, int k) {
         Stack<Character> st = new Stack<>();
 
         for(int i = 0; i < num.length(); i++){
@@ -30,5 +31,36 @@ class Solution {
         }
         
         return res.toString();
+    }
+
+    //This is the editorial solution
+    public String removeKdigits(String num, int k) {
+        LinkedList<Character> stack = new LinkedList<Character>();
+        
+        for(char digit : num.toCharArray()) {
+        while(stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+            stack.removeLast();
+            k -= 1;
+        }
+        stack.addLast(digit);
+        }
+            
+        /* remove the remaining digits from the tail. */
+        for(int i=0; i<k; ++i) {
+        stack.removeLast();
+        }
+            
+        // build the final string, while removing the leading zeros.
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        for(char digit: stack) {
+        if(leadingZero && digit == '0') continue;
+        leadingZero = false;
+        ret.append(digit);
+        }
+        
+        /* return the final string  */
+        if (ret.length() == 0) return "0";
+        return ret.toString();
     }
 }
