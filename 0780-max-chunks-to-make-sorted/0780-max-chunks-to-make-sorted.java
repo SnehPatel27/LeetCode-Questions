403185af7174ffba4a6b8b262d1e3c996ebe139d
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-03-31 15:14:55 -0700

    Time: 0 ms (100%), Space: 40.6 MB (95%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
@@ -14,3 +14,9 @@ class Solution {
         return chunks;
     }
 }
+
+/* Approach 2: Editorial 1: 
+new chunk at index i can only be created if all the numbers in the current and previous chunks (the "prefix" of the array) are smaller than all the numbers in the following chunks (the "suffix" of the array). This is equivalent to checking whether:
+
+max(prefix[0:i])<min(suffix[i:n]).
+*/
