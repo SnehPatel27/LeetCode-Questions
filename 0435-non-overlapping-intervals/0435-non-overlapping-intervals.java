65101212d68f26c21fdc5d74a616da5d24791e80
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-01 18:09:54 -0700

    Time: 48 ms (62.78%), Space: 73.4 MB (71.87%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,4 +1,5 @@
-class Solution {
+//My Approach
+class Solution1 {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0],b[0]));
         int length = intervals.length;
@@ -17,5 +18,24 @@ class Solution {
         }
 
         return count;
+    }
+}
+
+//Striver Approach
+//In this you make the problem similar to N meeting room where you need to find out how many meetings can be conducted in a single room. So in that case you sort the entire arry with the end time and then you figure how many non overlapping intervals are there and this you would subtract from the total intervals length.
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1],b[1]));
+        int length = intervals.length;
+        int count = 1;
+        int endTime = intervals[0][1];
+        for(int i = 1; i < length; i++){
+            if(intervals[i][0] >= endTime){
+                count++;
+                endTime = intervals[i][1];
+            }
+        }
+
+        return length - count;
     }
 }
