b11bf903da052cf2847926a8224fb7db3b84ba29
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-24 13:15:22 -0700

    Time: 37 ms (86.66%), Space: 101.1 MB (11.18%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,4 +1,5 @@
-class Solution {
+//This is my solution.
+class Solution1 {
     public int countDays(int days, int[][] meetings) {
         List<int[]> mergedMeetings = new ArrayList<>();
 
@@ -18,5 +19,32 @@ class Solution {
         }
 
         return days - totalMeetingDays;
+    }
+}
+
+//This is optimal Solution. Provide this one.
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        List<int[]> mergedMeetings = new ArrayList<>();
+
+        int lastEndTime = 0;
+        int freeDays = 0;
+
+        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0], b[0]));
+
+        for(int[] meeting: meetings){
+            
+            int start = meeting[0];
+            int end = meeting[1];
+
+            if(start > lastEndTime + 1){
+                freeDays += start - lastEndTime - 1;
+            }
+
+            lastEndTime = Math.max(lastEndTime, end);
+        }
+
+        freeDays += days - lastEndTime;
+        return freeDays;
     }
 }
