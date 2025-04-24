e78afeda44b1bebbb546f516edaa4831a1fb3d6a
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-24 11:54:58 -0700

    Time: 17 ms (9.94%), Space: 125.3 MB (21.73%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,27 +1,25 @@
 class Solution {
     public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n];
+        Arrays.fill(dp, -1);
+        return findMostPoints(questions, 0, dp);   
+    }
+
+    public long findMostPoints(int[][] questions, int index, long[] dp){
 
         int n = questions.length;
 
-        long[] dp = new long[n + 1];
-        Arrays.fill(dp, -1);
-
-        dp[n] = 0;
-
-        for(int i = n - 1; i >= 0; i--){
-            
-            int points = questions[i][0];
-            int steps = questions[i][1];
-
-            long notTake = dp[i + 1];
-            long take = 0;
-            if(i + steps + 1 <= n) 
-                take = points + dp[i + steps + 1];
-            else take = points;
-
-            dp[i] = Math.max(take, notTake);
+        if(index == n - 1){
+            return questions[index][0];
         }
+        if(index >= n) return 0;
 
-        return dp[0];
+        if(dp[index] != -1) return dp[index];
+
+        long notTake = findMostPoints(questions, index + 1, dp);
+        long take = questions[index][0] + findMostPoints(questions, index + questions[index][1] + 1, dp);
+
+        return dp[index] = Math.max(notTake, take);
     }
 }
