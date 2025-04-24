aa5effef7c71b815a1453df3a83629135dd14da3
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-23 20:23:01 -0700

    Time: 1 ms (99.78%), Space: 44.2 MB (55.25%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -11,20 +11,20 @@ class Solution {
             Arrays.fill(i, -1);
         }
 
-        return (triangle.get(0).get(0) + minimumTotalHelper(triangle, 1, 0, dp));
+        return minimumTotalHelper(triangle, 0, 0, dp);
     }
 
-    public int minimumTotalHelper(List<List<Integer>> triangle, int row, int prevIndex, int[][] dp){
+    public int minimumTotalHelper(List<List<Integer>> triangle, int row, int col, int[][] dp){
 
         if(row == triangle.size() - 1){
-            return Math.min(triangle.get(row).get(prevIndex), triangle.get(row).get(prevIndex + 1));
+            return triangle.get(row).get(col);
         }
 
-        if(dp[row][prevIndex] != -1) return dp[row][prevIndex];
+        if(dp[row][col] != -1) return dp[row][col];
         
-        int first = triangle.get(row).get(prevIndex) + minimumTotalHelper(triangle, row + 1, prevIndex, dp);
-        int second = triangle.get(row).get(prevIndex + 1) + minimumTotalHelper(triangle, row + 1, prevIndex + 1, dp);
+        int first = triangle.get(row).get(col) + minimumTotalHelper(triangle, row + 1, col, dp);
+        int second = triangle.get(row).get(col) + minimumTotalHelper(triangle, row + 1, col + 1, dp);
 
-        return dp[row][prevIndex] = Math.min(first, second);
+        return dp[row][col] = Math.min(first, second);
     }
 }
