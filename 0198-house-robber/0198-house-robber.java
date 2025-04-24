71fdcae549f36ddf0ef3f67c775232b4d4dbca6c
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-23 19:08:16 -0700

    Time: 0 ms (100%), Space: 41.6 MB (8.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int rob(int[] nums) {
         int dp[] = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
@@ -16,5 +16,22 @@ class Solution {
         int notPick = checkAdjacentElements(index + 1, nums, dp);
 
         return dp[index] = Math.max(pick, notPick);
+    }
+}
+
+class Solution {
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
+        
+        dp[0] = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            int notTake = 0 + dp[i - 1];
+            int take = i > 1 ? dp[i - 2] + nums[i] : nums[i];
+
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[nums.length - 1];
     }
 }
