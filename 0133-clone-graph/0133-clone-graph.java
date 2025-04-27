4e2d9eb18b8d41761c2b99224fd3bc160e434404
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-26 17:30:32 -0700

    Time: 38 ms (8.12%), Space: 43.2 MB (12.77%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -23,9 +23,11 @@ class Solution {
     HashMap<Node, Node> result = new HashMap<>();
 
     public Node cloneGraph(Node node) {
-        
+
         if(node == null)
             return node;
+
+        System.out.println(node.val);
 
         if (result.containsKey(node))
             return result.get(node);
@@ -40,3 +42,45 @@ class Solution {
         return cloneNode;
     }
 }
+
+/*
+Since Node 1 is not cloned yet, create a clone for Node 1 and put (Node 1 → cloneNode 1) into the result map.
+
+Now move to Node 1's neighbor, which is Node 2.
+
+Since Node 2 is not cloned yet, create a clone for Node 2 and put (Node 2 → cloneNode 2) into the result map.
+
+Now move to Node 2's neighbor, which is Node 1.
+
+Node 1 is already cloned, so just use the clone from the map and add it to cloneNode 2’s neighbors.
+
+Now move to Node 2's other neighbor, which is Node 3.
+
+Since Node 3 is not cloned yet, create a clone for Node 3 and put (Node 3 → cloneNode 3) into the result map.
+
+Now move to Node 3’s neighbor, which is Node 2.
+
+Node 2 is already cloned, so just use the clone from the map and add it to cloneNode 3’s neighbors.
+
+Now move to Node 3’s other neighbor, which is Node 4.
+
+Since Node 4 is not cloned yet, create a clone for Node 4 and put (Node 4 → cloneNode 4) into the result map.
+
+Now move to Node 4's neighbor, which is Node 1.
+
+Node 1 is already cloned, so just use the clone from the map and add it to cloneNode 4’s neighbors.
+
+Now move to Node 4's other neighbor, which is Node 3.
+
+Node 3 is already cloned, so just use the clone from the map and add it to cloneNode 4’s neighbors.
+
+Now all neighbors of Node 4 are processed. Go back to Node 3, add cloneNode 4 to cloneNode 3’s neighbors.
+
+Then go back to Node 2, add cloneNode 3 to cloneNode 2’s neighbors.
+
+Now back to Node 1, after handling Node 2, move to its other neighbor which is Node 4.
+
+Node 4 is already cloned, so just use the clone from the map and add it to cloneNode 1’s neighbors.
+
+Finally, return cloneNode 1. 
+*/
