208f59694628159a98994d85cfa75e1d947d17d1
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-27 01:01:12 -0700

    Time: 1 ms (71.51%), Space: 43.4 MB (39.37%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 
     public boolean isConnected(int source, int target, boolean[] visited, List<Integer>[] adj){
 
@@ -42,6 +42,69 @@ class Solution {
             
             adj[source].add(target);
             adj[target].add(source);
+        }
+
+        return new int[]{};
+    }
+}
+
+// ---------------------------------------------------------------------------------------
+
+class DisjointSet{
+    int[] size;
+    int[] parent;
+
+    DisjointSet(int n){
+        size = new int[n];
+        parent = new int[n];
+
+        for(int i = 0; i < n; i++){
+            size[i] = 0;
+            parent[i] = i;
+        }
+    }
+
+    public int findParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+
+        int ultimateParent = findParent(parent[node]);
+        parent[node] = ultimateParent;
+        return ultimateParent;
+    }
+
+    public void union(int node1, int node2){
+        int ultimateParentOf1 = parent[node1];
+        int ultimateParentOf2 = parent[node2];
+
+        if(ultimateParentOf1 == ultimateParentOf2){
+            return;
+        }
+
+        if(size[ultimateParentOf2] > size[ultimateParentOf1]){
+            parent[ultimateParentOf1] = ultimateParentOf2;
+            size[ultimateParentOf2] += size[ultimateParentOf1];
+        } else {
+            parent[ultimateParentOf2] = ultimateParentOf1;
+            size[ultimateParentOf1] += size[ultimateParentOf2];
+        }
+    }
+}
+
+class Solution{
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        DisjointSet ds = new DisjointSet(n + 1);
+        for(int[] edge: edges){
+            int node1 = edge[0];
+            int node2 = edge[1];
+
+            if(ds.findParent(node1) == ds.findParent(node2)){
+                return edge;
+            } else {
+                ds.union(node1, node2);
+            }
         }
 
         return new int[]{};
