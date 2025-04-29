c9a4e7d3b6f7d5f8a6fb4bc9c88a5fc9005727ad
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-28 22:55:28 -0700

    Time: 821 ms (5.02%), Space: 45.5 MB (7.73%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,5 +1,5 @@
 //One of the solution is to sort the shorter string s1 and then compare the window of sorted s1.length() characters in s2 and check if they are same or not. If yes return true or at the end return false. 
-class Solution {
+class Solution1 {
     public boolean checkInclusion(String s1, String s2) {
         
         s1 = sort(s1);
@@ -16,5 +16,43 @@ class Solution {
         char[] ch = s.toCharArray();
         Arrays.sort(ch);
         return new String(ch);
+    }
+}
+
+//Another Solution would be to create a hashMap for s1 and then for every window of s2 create a hashMap of s2 and at every intervals compare both the hashMaps. If they are same return true or else return false. 
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {   
+        HashMap<Character, Integer> s1Map = new HashMap<>();
+
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+
+        //Create s1Map
+        for(int i = 0; i < s1Len; i++){
+            s1Map.put(s1.charAt(i), s1Map.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+
+        for(int i = 0; i <= s2Len - s1Len; i++){
+            HashMap<Character, Integer> s2Map = new HashMap<>();
+            for(int j = 0; j < s1Len; j++){
+                s2Map.put(s2.charAt(i + j), s2Map.getOrDefault(s2.charAt(i + j), 0) + 1);
+            }
+
+            if(matches(s1Map, s2Map)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean matches(HashMap<Character, Integer> s1Map, HashMap<Character, Integer> s2Map){
+        for(char c: s1Map.keySet()){
+            if(s1Map.get(c) - s2Map.getOrDefault(c, 0) != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
