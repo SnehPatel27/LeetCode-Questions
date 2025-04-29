4ad6bc4f8c3c7e835c72e5a5639236c71c3eba44
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-29 00:35:03 -0700

    Time: 4 ms (97.34%), Space: 44.9 MB (68.89%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -72,6 +72,86 @@ class MinStack {
     }
 }
 
+//You can also use Two Stack Solution
+class MinStackk {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public MinStackk() {}
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+//You can optimize the two stack solution
+//So the issue with the above solution was there would be repeated values stored in the stack. For instance if 6 comes 5 times then in the min stack there would be 5 sixes stored and upon popping out there would be multiple pop operations too. So in the min stack instead of just storing the min value on the top you can store the frequency of the min number too. Upon popping you check if the number that i am popping as same as the top of the minstack and if yes then you decrement the count of the frequency by 1 and only pop if the frequency becomes 0. 
+class MinStackkk {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<int[]> minStack = new Stack<>();
+
+    public MinStackkk() {}
+
+    public void push(int x) {
+        // We always put the number onto the main stack.
+        stack.push(x);
+
+        // If the min stack is empty, or this number is smaller than
+        // the top of the min stack, put it on with a count of 1.
+        if (minStack.isEmpty() || x < minStack.peek()[0]) {
+            minStack.push(new int[] { x, 1 });
+        }
+        // Else if this number is equal to what's currently at the top
+        // of the min stack, then increment the count at the top by 1.
+        else if (x == minStack.peek()[0]) {
+            minStack.peek()[1]++;
+        }
+    }
+
+    public void pop() {
+        // If the top of min stack is the same as the top of stack
+        // then we need to decrement the count at the top by 1.
+        if (stack.peek().equals(minStack.peek()[0])) {
+            minStack.peek()[1]--;
+        }
+
+        // If the count at the top of min stack is now 0, then remove
+        // that value as we're done with it.
+        if (minStack.peek()[1] == 0) {
+            minStack.pop();
+        }
+
+        // And like before, pop the top of the main stack.
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek()[0];
+    }
+}
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
