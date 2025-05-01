7179639be01c25042857a4f37d229c2034fae3b1
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-30 23:56:23 -0700

    Time: 121 ms (39.85%), Space: 63.6 MB (69.48%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,39 @@
+//In this while inserting into the heap it is very important to follow the order. 
+//So in this case. My all elements in maxHeap would always be less then all elements in minHeap.
+class MedianFinder {
+
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((a,b) -> (b - a));
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {    
+        int minHeapSize = minHeap.size();
+        int maxHeapSize = maxHeap.size();
+
+        if((minHeapSize + maxHeapSize) % 2 == 0){
+            return (double)(minHeap.peek() + maxHeap.peek()) / 2;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
