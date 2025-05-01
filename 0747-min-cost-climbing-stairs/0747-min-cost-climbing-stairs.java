836998f9447984ce7731ae7a81ed69214ac2fae7
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-01 00:37:44 -0700

    Time: 0 ms (100%), Space: 43.5 MB (21.27%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,7 +1,7 @@
 //This is the recursive solution
 
 //This is the memoization solution
-class Solution {
+class Solution1 {
     public int minCostClimbingStairs(int[] cost) {
         
         int[] dp = new int[cost.length];
@@ -30,3 +30,21 @@ class Solution {
 }
 
 //This is the tabulation solution
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        
+        int[] dp = new int[cost.length];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < cost.length; i++){
+            int Step1Cost = cost[i] + dp[i - 1];
+            int Step2Cost = cost[i] + dp[i - 2];
+
+            dp[i] = Math.min(Step1Cost, Step2Cost);
+        }
+
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+}
