76d0d592e61a1ee7454ed0df7e454f74d8bec855
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-04-30 17:02:02 -0700

    Time: 1 ms (57%), Space: 41.4 MB (8.42%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,3 +1,4 @@
+//Every number can be written as the product of prime number. In this case you only need to see if the number can be achived by multiplying 2,3,5. So just keep on dividing the number by them till it is possible and at the end check if n == 1 or we have been able to successfully divide the number and reached 1. If yes return true or else return false. 
 class Solution {
     public boolean isUgly(int n) {
 
