cb478aa3c7cd5890a44303cc1053b94a5bf074fa
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-01 01:21:45 -0700

    Time: 0 ms (100%), Space: 40.5 MB (61.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,60 @@
+//This is the recursive Solution
+class Solution1 {
+    public int uniquePaths(int m, int n) {
+        return generateUniquePaths(m, n, 0, 0);
+    }
+
+    public int generateUniquePaths(int m, int n, int currRow, int currCol){
+        
+        if(currRow < 0 || currRow >= m || currCol < 0 || currCol >= n){
+            return 0;
+        }
+        
+        if(currRow == m - 1 && currCol == n - 1){
+            return 1;
+        }
+
+        int down = generateUniquePaths(m, n, currRow + 1, currCol);
+        int right = generateUniquePaths(m, n, currRow, currCol + 1);
+
+        return down + right;
+    }
+}
+
+//This is the memoization Solution
 class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int[][] dp = new int[m][n];
+
+        for(int[] rows: dp){
+            Arrays.fill(rows, -1);
+        }
+
+        return generateUniquePaths(m, n, 0, 0, dp);
+    }
+
+    public int generateUniquePaths(int m, int n, int currRow, int currCol, int[][] dp){
+        
+        if(currRow < 0 || currRow >= m || currCol < 0 || currCol >= n){
+            return 0;
+        }
+        
+        if(currRow == m - 1 && currCol == n - 1){
+            return 1;
+        }
+
+        if(dp[currRow][currCol] != -1) return dp[currRow][currCol];
+
+        int down = generateUniquePaths(m, n, currRow + 1, currCol, dp);
+        int right = generateUniquePaths(m, n, currRow, currCol + 1, dp);
+
+        return dp[currRow][currCol] = down + right;
+    }
+}
+
+//This is the DP solution
+class Solution2 {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
         dp[0][0] = 1;
