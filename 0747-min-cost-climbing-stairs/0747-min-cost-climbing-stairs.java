b77be5d4eb045f662116bf42be86c7ff6f628d22
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-01 00:29:30 -0700

    Time: 1 ms (38%), Space: 43.4 MB (28.76%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,3 +1,6 @@
+//This is the recursive solution
+
+//This is the memoization solution
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
@@ -5,26 +8,25 @@ class Solution {
 
         Arrays.fill(dp, -1);
         
-        int startAt0 = findMinCost(cost, cost.length, 0, dp);
-        int startAt1 = findMinCost(cost, cost.length, 1, dp);
-
-        System.out.println(startAt0);
-        System.out.println(startAt1);
+        int startAt0 = findMinCost(cost, 0, dp);
+        int startAt1 = findMinCost(cost, 1, dp);
 
         return Math.min(startAt0, startAt1);
     }
 
-    public int findMinCost(int[] cost, int n, int index, int[] dp){
+    public int findMinCost(int[] cost, int index, int[] dp){
         
-        if(index >= n){
+        if(index >= cost.length){
             return 0;
         }
 
         if(dp[index] != -1) return dp[index];
 
-        int Step1Cost = cost[index] + findMinCost(cost, n, index + 1, dp);
-        int Step2Cost = cost[index] + findMinCost(cost, n, index + 2, dp);
+        int Step1Cost = cost[index] + findMinCost(cost, index + 1, dp);
+        int Step2Cost = cost[index] + findMinCost(cost, index + 2, dp);
 
         return dp[index] = Math.min(Step1Cost, Step2Cost);
     }
 }
+
+//This is the tabulation solution
