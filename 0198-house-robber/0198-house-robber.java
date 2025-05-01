1c1f9d201babe88ab9330f928b6822012343d57a
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-01 00:53:59 -0700

    Time: 0 ms (100%), Space: 41 MB (76.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,25 +1,43 @@
+//This is the recursive solution
 class Solution1 {
     public int rob(int[] nums) {
+        return checkAdjacentElements(nums.length - 1, nums);
+    }
+
+    public int checkAdjacentElements(int index, int[] nums){
+        if(index == 0) return nums[index];
+        if(index < 0) return 0;
+
+        int notPick = checkAdjacentElements(index - 1, nums);
+        int pick = nums[index] + checkAdjacentElements(index - 2, nums);
+
+        return Math.max(pick, notPick);
+    }
+}
+
+//This is memoization
+class Solution {
+    public int rob(int[] nums) {
         int dp[] = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            dp[i] = -1;
-        }
-        return checkAdjacentElements(0 , nums, dp);
+        Arrays.fill(dp, -1);
+        return checkAdjacentElements(nums.length - 1 , nums, dp);
     }
 
     public int checkAdjacentElements(int index, int[] nums, int[] dp){
-        if(index >= nums.length) return 0;
+        if(index == 0) return nums[index];
+        if(index < 0) return 0;
 
         if(dp[index] != -1) return dp[index];
 
-        int pick = nums[index] + checkAdjacentElements(index + 2, nums, dp);
-        int notPick = checkAdjacentElements(index + 1, nums, dp);
+        int pick = nums[index] + checkAdjacentElements(index - 2, nums, dp);
+        int notPick = checkAdjacentElements(index - 1, nums, dp);
 
         return dp[index] = Math.max(pick, notPick);
     }
 }
 
-class Solution {
+//This is DP
+class Solution2 {
     public int rob(int[] nums) {
         int dp[] = new int[nums.length];
         
