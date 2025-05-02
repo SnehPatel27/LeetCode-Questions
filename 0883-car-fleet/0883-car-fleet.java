11c82229cdd6fba8070f17854839349a1e56c9df
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-01 17:49:00 -0700

    Time: 65 ms (85.07%), Space: 57.7 MB (52.39%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.java
@@ -0,0 +1,34 @@
+class Solution {
+    //If you sort the positions array. The last element would be the one that would be the closest to the target. Just compare iteratively the last two cars time to reach. If the second last car reaches early despite it being behind the last car then that mean both the cards would meet somewhere and then they both would travel at the speed of the last car because the speed of that car would be less. 
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        Cars[] cars = new Cars[n];
+
+        for(int i = 0; i < n; i++){
+            cars[i] = new Cars(position[i], (double) (target - position[i]) / speed[i]);
+        }
+
+        Arrays.sort(cars, (a, b) -> Integer.compare(a.position, b.position));
+
+        int t = n;
+        int ans = 0;
+
+        while(--t > 0){
+            if(cars[t].time < cars[t - 1].time) ans++;            
+            else cars[t - 1] = cars[t];
+        }
+            
+        return ans + (t == 0 ? 1 : 0);
+    }
+}
+
+public class Cars{
+    int position;
+    double time;
+
+    Cars(int position, double time){
+        this.position = position;
+        this.time = time;
+    }
+}
