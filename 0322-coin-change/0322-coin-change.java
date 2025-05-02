0d4c4ed22fce642cafb0b4629db3bf4586caf551
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-02 13:09:15 -0700

    Time: 19 ms (40.18%), Space: 45.7 MB (12.8%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,8 +1,4 @@
-class Solution {
-
-    //First There would be a solution of TLE that I recently did. Show me that one.
-    //Then Show me this solution of memoization
-    //Then show me the one with DP that was submitted on 1st Novemember
+class Solution1 {
 
     public int coinChange(int[] coins, int amount) {
         int dp[][] = new int[coins.length][amount + 1];
@@ -37,7 +33,36 @@ class Solution {
 
         return dp[index][target] = Math.min(take, notTake);
     }
+}
 
-    //If the constraints are simple and the coins[i] value is length and also the length of the coins is less
-    //Then please provide me some other solution if applicable.
+//This is the DP solution
+public class Solution{
+    public int coinChange(int[] coins, int amount) {
+        int dp[][] = new int[coins.length][amount + 1];
+
+        for(int i = 0; i < amount + 1; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            } else{
+                dp[0][i] = Integer.MAX_VALUE;
+            }
+        }
+
+        for(int i = 1; i < coins.length; i++){
+            for(int j = 0; j < amount + 1; j++){
+                int notTake = dp[i - 1][j];
+
+                int take = Integer.MAX_VALUE;
+                if(coins[i] <= j){
+                    int subResult = dp[i][j - coins[i]];
+                    if(subResult != Integer.MAX_VALUE){
+                        take = 1 + subResult;
+                    }
+                }
+                dp[i][j] = Math.min(take, notTake);
+            }
+        }
+
+        return dp[coins.length - 1][amount] == Integer.MAX_VALUE ? -1 : dp[coins.length - 1][amount];
+    }
 }
