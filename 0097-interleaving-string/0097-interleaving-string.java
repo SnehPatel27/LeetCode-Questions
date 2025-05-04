8ce145541e72e072367e32123d942723d3bc30a5
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-03 23:07:17 -0700

    Time: 7 ms (29.77%), Space: 41.7 MB (70.76%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,4 +1,5 @@
-class Solution {
+//This is the recursion with memoization solution
+class Solution1 {
     public boolean isInterleave(String s1, String s2, String s3) {
 
         if(s1.length() + s2.length() != s3.length()) return false;
@@ -34,5 +35,54 @@ class Solution {
         }
 
         return dp[index1][index2] = Math.max(firstString, secondString);
+    }
+}
+
+//This is the DP solution. 
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        if(s1.length() + s2.length() != s3.length()) return false;
+
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+
+        //Index1 - 0 - s1.length()
+        //Index2 - 0 - s2.length()
+
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+
+        dp[s1.length()][s2.length()] = true;
+
+        for (int i = s1Len - 1; i >= 0; i--) {
+            if (s1.charAt(i) == s3.charAt(i + s2Len) && dp[i + 1][s2Len]) {
+                dp[i][s2Len] = true;
+            }
+        }
+
+        for (int j = s2Len - 1; j >= 0; j--) {
+            if (s2.charAt(j) == s3.charAt(s1Len + j) && dp[s1Len][j + 1]) {
+                dp[s1Len][j] = true;
+            }
+        }
+
+        for(int i = s1Len - 1; i >= 0; i--){
+            for(int j = s2Len - 1; j >= 0; j--){
+
+                boolean firstString = false, secondString = false;
+
+                if(i < s1Len && s1.charAt(i) == s3.charAt(i + j)){
+                    firstString = dp[i + 1][j];
+                }
+
+                if(j < s2Len && s2.charAt(j) == s3.charAt(i + j)){
+                    secondString = dp[i][j + 1];
+                }
+
+                dp[i][j] = firstString || secondString;
+            }
+        }
+
+        return dp[0][0];
     }
 }
