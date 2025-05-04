e78ae205865d74c5079a69634acd1c90e7c33108
Sneh Patel <76803926+SnehPatel27@users.noreply.github.com>
2025-05-03 23:57:51 -0700

    Time: 46 ms (40.55%), Space: 46.9 MB (38.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,4 +1,36 @@
-class Solution {
+//This is the recursive solution
+class Solution1 {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int totalSum = 0;
+        for(int i = 0; i < n; i++){
+            totalSum += nums[i];
+        }
+
+        if(totalSum % 2 != 0) return false;
+
+        return canPartitionHelper(nums, n - 1, totalSum / 2);
+    }
+
+    public boolean canPartitionHelper(int[] nums, int index, int target){
+
+        if(index == 0){
+            if(target == 0 || nums[0] == target) return true;
+            return false;
+        }
+
+        boolean notPick = canPartitionHelper(nums, index - 1, target);
+        boolean pick = false;
+        if(nums[index] <= target){
+            pick = canPartitionHelper(nums, index - 1, target - nums[index]);
+        }
+
+        return pick || notPick;
+    }
+}
+
+//This is the recursion with memoization solution
+class Solution2 {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int totalSum = 0;
@@ -33,5 +65,40 @@ class Solution {
         }
 
         return dp[index][target] = Math.max(pick, notPick);
+    }
+}
+
+//This is the dp solution
+public class Solution{
+    public boolean canPartition(int[] nums) {
+        
+        int n = nums.length;
+        int totalSum = 0;
+        for(int i = 0; i < n; i++){
+            totalSum += nums[i];
+        }
+
+        if(totalSum % 2 != 0) return false;
+
+        int target = totalSum / 2;
+
+        boolean[][] dp = new boolean[n][target + 1];
+
+        for(int i = 0; i <= target; i++){
+            if(i == 0 || nums[0] == i) dp[0][i] = true;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= target; j++){
+                boolean notPick = dp[i - 1][j];
+                boolean pick = false;
+                if(nums[i] <= j){
+                    pick = dp[i - 1][j - nums[i]];
+                }
+
+                dp[i][j] = pick || notPick;
+            }
+        }
+        return dp[n - 1][target];
     }
 }
